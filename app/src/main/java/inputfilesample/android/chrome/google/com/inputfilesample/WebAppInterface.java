b0116cffb4f5ef82a39e98c364d8f24adf3faf40
Carlo Alberto Negri <calber@calber.org>
2016-07-22 10:01:27 +0200

    if

---
+++ app/src/main/java/inputfilesample/android/chrome/google/com/inputfilesample/WebAppInterface.java
@@ -0,0 +1,22 @@
+package inputfilesample.android.chrome.google.com.inputfilesample;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+/**
+ * Created by calber on 21/7/16.
+ */
+public class WebAppInterface {
+    private Context mContext;
+
+    public WebAppInterface(Context context) {
+        mContext = context;
+    }
+
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+}
